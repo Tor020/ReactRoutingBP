2b22030db94526263e4285b5a91629e1d514601f
= <=>
2017-12-29 22:00:32 -0500

    'msg'

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Main from "./components/Main";
+import "./components/styles/index.css";
+
+
+ReactDom.render(
+    <Main/>,
+    document.getElementById('root')
+);
